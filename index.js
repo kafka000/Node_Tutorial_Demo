47410361c82e2a0829a3be9938a8e778477d355e
liyanjun <kafka673@gmail.com>
2024-05-20 18:25:46 +0800

    通过key/value处理路由

--- index.js
+++ index.js
@@ -4,5 +4,17 @@
 var server = require("./server");
 //3.3 扩展index.js，使得路由函数可以被注入到服务器
 var router = require("./router");
+//4.1 依赖注入 引用requestHandlers模块
+var requestHandlers = require("./requesthandlers");
 
-server.start(router.route);
+
+//4.2 将一系列请求处理程序通过一个对象来传递，并且需要使用松耦合的方式将这个对象注入到route()函数中。
+// 在C++或C#中，当我们谈到对象，指的是类或者结构体的实例。对象根据他们实例化的模板（就是所谓的类），会拥有不同的 属性和方法。但在JavaScript里对象不是这个概念。
+// 在JavaScript中，对象就是一个键/值对的集合 -- 你可以把JavaScript的对象想象成一个键为字符串类型的字典。
+var handle ={}
+handle["/"]= requestHandlers.start;
+handle["/start"]= requestHandlers.start;
+handle["/upload"]= requestHandlers.upload;
+
+
+server.start(router.route,handle);
--- server.js
+++ server.js
@@ -7,16 +7,17 @@ var http = require("http");
 //2.1 引用url模块
 var url = require("url");
 //3.2 依赖注入 引用router模块
-const { route } = require("./router");
+var route = require("./router");
 
 //1.2 我们把我们的服务器脚本放到一个叫做 start 的函数里，然后我们会导出这个函数。
-function start(){
+function start(route, handle){
   // 1.3 接下来我们调用http模块提供的函数： createServer 。
   function onRequest(request, response){
     //2.2 通过请求的URL路径来区别不同请求
     var pathname = url.parse(request.url).pathname
     //3.2 我们将使用依赖注入的方式较松散地添加路由模块
-    route(pathname);
+    //4.2 这样我们就在start()函数里添加了handle参数，并且把handle对象作为第一个参数传递给了route()回调函数。
+    route(handle,pathname);
 
     console.log("Request for "+ pathname +" received.");    
     response.writeHead(200,{"Content-Type":"text/plain"});
